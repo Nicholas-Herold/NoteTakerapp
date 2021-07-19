a1ca11c86f213ab9cabb5905028c065d7610fa65
Nicholas Herold <nherold86@gmail.com>
2021-07-18 20:24:18 -0400

    basic functions working

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -1,10 +1,33 @@
-const path = require('path');
-let datatable = require('../db/db.json')
 
+
+const { response } = require('express');
+const fs = require('fs');
 module.exports = function(app){
+    let datatable = require('../db/db.json');
+    app.get('/api/notes', (req,res) =>{
+        console.log('1')
+
+        let dbnotes = JSON.parse( fs.readFileSync('./db/db.json','utf8'));
+        res.json(dbnotes)
+
+    });
 
     app.get('/api/data', function(req,res){
         res.json(datatable);
-    })
+    });
+
+    app.post('/api/notes', (req,res)=>{
+        
+        
+        let info = req.body
+        let dbnotes = JSON.parse( fs.readFileSync('./db/db.json','utf8'));
+       
+        
+        dbnotes.push(info);
+        
+        fs.writeFileSync('./db/db.json', JSON.stringify(dbnotes) );
+       
+        
+    });
 
 };
